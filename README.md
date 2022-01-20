26623443df05d79b801816ec92919718ba20eb94
Gergo Huszty <gergo.huszty@digitaltrip.hu>
2022-01-20 22:15:52 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# This is a very WorkInProgress project.
+
+## Smart LEDs matrix
+A `DrawTarget` implementation to use (one, or more) smart LED matrixes as a graphics display driven by embedded-graphics Dwarable `objects`.
---
+++ src/lib.rs
@@ -0,0 +1,71 @@
+#![no_std]
+
+use embedded_graphics::prelude::PointsIter;
+use embedded_graphics_core::{
+    draw_target::DrawTarget,
+    geometry::Size,
+    geometry::{Dimensions, OriginDimensions},
+    pixelcolor::*,
+    Pixel,
+};
+use display_interface::DisplayError;
+
+//use smart_leds_trait::{SmartLedsWrite, RGB8};
+use smart_leds::{SmartLedsWrite, hsv::RGB8};
+use ws2812_spi as ws2812;
+pub struct SmartLedMatrix<T> {
+    writer: ws2812::Ws2812<T>,
+    content: [RGB8; 64]
+}
+
+
+impl<T: embedded_hal::spi::FullDuplex<u8>> OriginDimensions for SmartLedMatrix<T> {
+    fn size(&self) -> Size {
+        Size::new(8, 8)
+    }
+}
+
+
+impl<T: embedded_hal::spi::FullDuplex<u8>> SmartLedMatrix<T> {
+    pub fn new(writer: ws2812::Ws2812<T>) -> Self {
+            Self{writer: writer,
+                content: [RGB8::default(); 64]}
+    }
+}
+
+impl<T: embedded_hal::spi::FullDuplex<u8>> DrawTarget for SmartLedMatrix<T> {
+    type Color = Rgb888;
+    type Error = DisplayError;
+
+    fn draw_iter<I>(&mut self, pixels: I) -> Result<(), Self::Error>
+    where
+    I: IntoIterator<Item = Pixel<Rgb888>> {
+        pixels.into_iter().for_each(|Pixel(pos, color)| {
+            if pos.x >= 0 && pos.x <= 7 && pos.y >= 0 && pos.y <= 7 {
+                self.content[(pos.x*8+pos.y) as usize] = RGB8::new(color.r(), color.g(), color.b());
+            }
+        });
+        self.writer.write(self.content.iter().cloned());
+        Ok(())
+    }
+
+    fn fill_contiguous<I>(&mut self, area: &embedded_graphics::primitives::Rectangle, colors: I) -> Result<(), Self::Error>
+    where
+        I: IntoIterator<Item = Self::Color>,
+    {
+        self.draw_iter(
+            area.points()
+                .zip(colors)
+                .map(|(pos, color)| Pixel(pos, color)),
+        )
+    }
+
+    fn fill_solid(&mut self, area: &embedded_graphics::primitives::Rectangle, color: Self::Color) -> Result<(), Self::Error> {
+        self.fill_contiguous(area, core::iter::repeat(color))
+    }
+
+    fn clear(&mut self, color: Self::Color) -> Result<(), Self::Error> {
+        self.fill_solid(&self.bounding_box(), color)
+    }
+
+}
