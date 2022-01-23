20338f000235d9e509139fcaadcb896e935df0c1
Gergo Huszty <gergo.huszty@digitaltrip.hu>
2022-01-23 14:43:55 +0100

    attempting to use pluggable array size to store screen content
    using generic_const_exprs results in the following (ton of) errors:
    
    ```
    error[E0412]: cannot find type `Option` in this scope
       --> /Users/gergo/.cargo/registry/src/github.com-1ecc6299db9ec823/memchr-2.3.4/src/lib.rs:442:56
        |
    442 |     fn imp(n1: u8, n2: u8, n3: u8, haystack: &[u8]) -> Option<usize> {
        |                                                        ^^^^^^ not found in this scope
        |
    help: consider importing this enum
        |
    43  | use core::option::Option;
        |
    ```

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,37 @@
 #![no_std]
+#![feature(generic_const_exprs)]
 
-use embedded_graphics_core::{
-    draw_target::DrawTarget,
-    geometry::Size,
-    geometry::{Dimensions, OriginDimensions},
-    pixelcolor::*,
-    Pixel,
-    primitives::Rectangle,
-    prelude::PointsIter,
-};
+use embedded_graphics_core::{Pixel, draw_target::DrawTarget, geometry::Size, geometry::{Dimensions, OriginDimensions}, pixelcolor::*, prelude::{Point, PointsIter}, primitives::Rectangle};
 use display_interface::DisplayError;
 
 use smart_leds::{SmartLedsWrite, hsv::RGB8};
 
-pub struct SmartLedMatrix<T> {
+pub struct SmartLedMatrix<T, M: MatrixType> 
+where [(); M::PIXELS]: {
     writer: T,
-    content: [RGB8; 64]
+    content: [RGB8; M::PIXELS],
+    matrix_type: M
 }
 
-
-impl<T: SmartLedsWrite> OriginDimensions for SmartLedMatrix<T> {
+impl<T: SmartLedsWrite, M: MatrixType> OriginDimensions for SmartLedMatrix<T, M> 
+where [(); M::PIXELS]: {
     fn size(&self) -> Size {
-        Size::new(8, 8)
+        self.matrix_type.size()
     }
 }
 
-
-impl<T: SmartLedsWrite> SmartLedMatrix<T> {
-    pub fn new(writer: T) -> Self {
-            Self{writer: writer,
-                content: [RGB8::default(); 64]}
+impl<T: SmartLedsWrite, M: MatrixType> SmartLedMatrix<T, M> 
+where [(); M::PIXELS]: {
+    pub fn new(writer: T, matrix_type: M) -> Self {
+        Self{writer: writer,
+            content: [RGB8::default(); M::PIXELS],
+            matrix_type: matrix_type}
     }
 }
 
-impl<T: SmartLedsWrite> DrawTarget for SmartLedMatrix<T> 
-where <T as SmartLedsWrite>::Color: From<RGB8> {
+impl<T: SmartLedsWrite, M: MatrixType> DrawTarget for SmartLedMatrix<T, M> 
+where <T as SmartLedsWrite>::Color: From<RGB8>,
+[(); M::PIXELS]: {
     type Color = Rgb888;
     type Error = DisplayError;
 
@@ -42,8 +39,8 @@ where <T as SmartLedsWrite>::Color: From<RGB8> {
     where
     I: IntoIterator<Item = Pixel<Rgb888>> {
         pixels.into_iter().for_each(|Pixel(pos, color)| {
-            if pos.x >= 0 && pos.x <= 7 && pos.y >= 0 && pos.y <= 7 {
-                self.content[(pos.x*8+(7-pos.y)) as usize] = RGB8::new(color.r(), color.g(), color.b());
+            if self.matrix_type.position_valid(pos) {
+                self.content[self.matrix_type.map(pos.x, pos.y)] = RGB8::new(color.r(), color.g(), color.b());
             }
         });
         //TODO: always returns an SPI overrun error on my stm32f401 
@@ -75,5 +72,30 @@ where <T as SmartLedsWrite>::Color: From<RGB8> {
     fn clear(&mut self, color: Self::Color) -> Result<(), Self::Error> {
         self.fill_solid(&self.bounding_box(), color)
     }
+}
 
+pub trait MatrixType {
+    fn map(&self, x: i32, y: i32) -> usize;
+    fn position_valid(&self, pos: Point) -> bool;
+    fn size(&self) -> Size;
+    const PIXELS: usize;
+}
+
+pub struct MT8x8 {
+}
+
+impl MatrixType for MT8x8 {
+    fn map(&self, x: i32, y: i32) -> usize {
+        (x*8+(7-y)) as usize
+    }
+
+    fn size(&self) -> Size {
+        Size::new(8, 8)
+    }
+
+    const PIXELS: usize = 64;
+
+    fn position_valid(&self, pos: Point) -> bool {
+        pos.x >= 0 && pos.x <= 7 && pos.y >= 0 && pos.y <= 7
+    }
 }
